eb4abd931db135665fc2b1f1fbc6061b7a098e7a
cristian <rodri07crisss@gmail.com>
2020-02-10 20:48:46 -0400

    primer comentario

---
+++ prueba.js
@@ -0,0 +1,9 @@
+"use strict";
+// alert('hola');
+function saludar(nombre) {
+    console.log("Hola " + nombre.toUpperCase());
+}
+var wolverine = {
+    nombre: "Logan"
+};
+saludar(wolverine.nombre);
